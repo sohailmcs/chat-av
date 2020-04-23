b8761a5a8b2541fa79e3e383402898b62c6cb7c1
Faisalarif <faisalariftdm@gmail.com>
2020-04-23 22:27:30 +0500

    confirmation dialog box

--- public/js/components-modal.js
+++ public/js/components-modal.js
@@ -9,33 +9,47 @@
     Author URL: hhttp://www.themeforest.net/user/pixinvent
 ==========================================================================================*/
 (function (window, document, $) {
-  'use strict';
+  "use strict";
 
   // onShow event
-  $('#onshowbtn').on('click', function () {
-    $('#onshow').on('show.bs.modal', function () {
-      alert('onShow event fired.');
+  $("#onshowbtn").on("click", function () {
+    $("#onshow").on("show.bs.modal", function () {
+      alert("onShow event fired.");
     });
   });
 
   // onShown event
-  $('#onshownbtn').on('click', function () {
-    $('#onshown').on('shown.bs.modal', function () {
-      alert('onShown event fired.');
+  $("#onshownbtn").on("click", function () {
+    $("#onshown").on("shown.bs.modal", function () {
+      alert("onShown event fired.");
     });
   });
 
   // onHide event
-  $('#onhidebtn').on('click', function () {
-    $('#onhide').on('hide.bs.modal', function () {
-      alert('onHide event fired.');
+  $("#onhidebtn").on("click", function () {
+    $("#onhide").on("hide.bs.modal", function () {
+      alert("onHide event fired.");
     });
   });
 
   // onHidden event
-  $('#onhiddenbtn').on('click', function () {
-    $('#onhidden').on('hidden.bs.modal', function () {
-      alert('onHidden event fired.');
+  $("#onhiddenbtn").on("click", function () {
+    $("#onhidden").on("hidden.bs.modal", function () {
+      alert("onHidden event fired.");
     });
   });
 })(window, document, jQuery);
+
+function hideRequestCallModal() {
+  document.getElementById("reqcall").style.display = "none";
+  $("#reqcall").modal("hide");
+  setTimeout(() => {
+    //document.getElementById("notificationmodal").style.display = "block";
+    $("#notificationmodal").modal("show");
+    
+  }, 3000);
+}
+
+function hideconfirmModel() {
+  $("#notificationmodal").modal("hide");
+}
