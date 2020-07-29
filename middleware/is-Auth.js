0f19dfcd390c276e39cfd18040a2692b3c38f13a
sohailmcs <sohail.mcs@gmail.com>
2020-07-30 02:58:34 +0500

    newchanges

--- middleware/is-Auth.js
+++ middleware/is-Auth.js
@@ -2,6 +2,10 @@ function isAuthorization(req, res, next) {
   if (!req.cookies.kindahUserName) {
     return res.redirect("/login");
   }
+  res.set(
+    "Cache-Control",
+    "no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0"
+  );
   next();
 }
 
@@ -11,6 +15,10 @@ function isLoginExist(req, res, next) {
       return res.redirect("/patientDashboard");
     else return res.redirect("/docDashboard");
   }
+  res.set(
+    "Cache-Control",
+    "no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0"
+  );
   next();
 }
 
