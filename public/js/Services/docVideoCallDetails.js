559edee4094e68bb123d2a0b2f8267fe220dfb93
sohailmcs <sohail.mcs@gmail.com>
2020-08-11 04:54:26 +0500

    push

--- public/js/Services/docVideoCallDetails.js
+++ public/js/Services/docVideoCallDetails.js
@@ -135,31 +135,40 @@ function handleError(error) {
 }
 
 function initializeSession() {
-  /* global OT, apiKey, sessionId, token */
-
-  // Initialize an OpenTok Session object
   var session = OT.initSession(apiKey, sessionId);
 
-  // Initialize a Publisher, and place it into the element with id="publisher"
-  var publisher = OT.initPublisher("publisher");
-
-  // Attach event handlers
-  session.on({
-    // This function runs when session.connect() asynchronously completes
-    sessionConnected: function () {
-      // Publish the publisher we initialzed earlier (this will trigger 'streamCreated' on other
-      // clients)
-      session.publish(publisher);
-    },
-
-    // This function runs when another client publishes a stream (eg. session.publish())
-    streamCreated: function (event) {
-      session.subscribe(event.stream, "subscribers", { insertMode: "append" });
-    },
+  // Subscribe to a newly created stream
+  session.on("streamCreated", function streamCreated(event) {
+    var subscriberOptions = {
+      insertMode: "append",
+      width: "100%",
+      height: "100%",
+    };
+    session.subscribe(
+      event.stream,
+      "subscriber",
+      subscriberOptions,
+      handleError
+    );
   });
 
-  // Connect to the Session using the 'apiKey' of the application and a 'token' for permission
-  session.connect(token);
+  // initialize the publisher
+  var publisherOptions = {
+    insertMode: "append",
+    width: "100%",
+    height: "100%",
+  };
+  var publisher = OT.initPublisher("publisher", publisherOptions, handleError);
+
+  // Connect to the session
+  session.connect(token, function callback(error) {
+    if (error) {
+      handleError(error);
+    } else {
+      // If the connection is successful, publish the publisher to the session
+      session.publish(publisher, handleError);
+    }
+  });
 }
 
 function performCall() {
