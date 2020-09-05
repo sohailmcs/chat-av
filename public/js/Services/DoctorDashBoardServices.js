70e717fc70231ebc66653d1e5eb2f73406caf53e
sohailmcs <sohail.mcs@gmail.com>
2020-09-06 03:49:46 +0500

    push

--- public/js/Services/DoctorDashBoardServices.js
+++ public/js/Services/DoctorDashBoardServices.js
@@ -1,5 +1,5 @@
-//var baseURL = "https://kindahclinic.com/KindahService/";
-var baseURL = "http://localhost:1042/KindahService/";
+var baseURL = "https://kindahclinic.com/KindahService/";
+//var baseURL = "http://localhost:1042/KindahService/";
 var options = {
   year: "numeric",
   month: "numeric",
