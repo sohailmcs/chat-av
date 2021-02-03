46d4bbd3297473d434d1031e65d7fd35f9f59ebb
sohailmcs <sohail.mcs@gmail.com>
2021-02-04 00:43:40 +0500

    push

--- services/docVideoCallDetails.js
+++ services/docVideoCallDetails.js
@@ -407,12 +407,12 @@ function countTimer() {
 //============calculate calling time==============
 
 function streamDestroyed(event) {
- // event.preventDefault();
- AudioVideosession.disconnect(); 
- AudioVideosession.destroy();
- AudioVideosession.unpublish(publisher);
-                 console.log("The publisher stopped streaming. Reason: "
-                  + event.reason);
+  event.preventDefault();
+//  AudioVideosession.disconnect(); 
+//  AudioVideosession.destroy();
+//  AudioVideosession.unpublish(publisher);
+//                  console.log("The publisher stopped streaming. Reason: "
+//                   + event.reason);
 }
 
 function disconnect() {
