f2ba65428f70870e07ca1de6255cff5e8d59aefd
sohailmcs <sohail.mcs@gmail.com>
2021-03-18 03:50:17 +0500

    push

--- services/Admin/ManageSuggestions.js
+++ services/Admin/ManageSuggestions.js
@@ -1,190 +1,192 @@
 //var baseURL = "https://kindahclinic.com/KindahService/";
 //var baseURL = "http://localhost:1042/KindahService/";
 
-$(function () {
-  GetAllSuggestions();
-  $(".btnSendReply").on("click", function () {
-    SendReply($("#hdnSuggestionId").val());
-  });
+$(function() {
+    GetAllSuggestions();
+    $(".btnSendReply").on("click", function() {
+        SendReply($("#hdnSuggestionId").val());
+    });
 }); //====end of $function
 
 function GetAllSuggestions() {
-  var url = baseURL + "PatientSuggestion/GetAllSuggestion";
-  ///==============start post request to add doctor
-  $.ajax({
-    url: url,
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    type: "GET",
-    datatype: "application/json",
-    contentType: "application/json; charset=utf-8",
-    data: "",
-    beforeSend: function () {
-      $.LoadingOverlay("show");
-    },
-    success: function (data, textStatus, xhr) {
-      $.LoadingOverlay("hide");
-      Filldatatable(data);
-    },
-    error: function (xhr, textStatus, err) {
-      if (xhr.status == "500" && xhr.statusText == "InternalServerError")
-        console.log(xhr.statusText);
-      else console.log(xhr.statusText);
-    },
-    complete: function (data) {
-      // Hide Loading
-      $.LoadingOverlay("hide");
-    },
-  });
+    var url = baseURL + "PatientSuggestion/GetAllSuggestion";
+    ///==============start post request to add doctor
+    $.ajax({
+        url: url,
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        type: "GET",
+        datatype: "application/json",
+        contentType: "application/json; charset=utf-8",
+        data: "",
+        beforeSend: function() {
+            $.LoadingOverlay("show");
+        },
+        success: function(data, textStatus, xhr) {
+            $.LoadingOverlay("hide");
+            Filldatatable(data);
+        },
+        error: function(xhr, textStatus, err) {
+            if (xhr.status == "500" && xhr.statusText == "InternalServerError")
+                console.log(xhr.statusText);
+            else console.log(xhr.statusText);
+        },
+        complete: function(data) {
+            // Hide Loading
+            $.LoadingOverlay("hide");
+        },
+    });
 }
 
 function ShowDetails(id) {
-  var suggestionId = $(id).attr("suggestionId");
-  $("#hdnSuggestionId").val(suggestionId);
-  GetSuggestionDetails(suggestionId);
+    var suggestionId = $(id).attr("suggestionId");
+    $("#hdnSuggestionId").val(suggestionId);
+    GetSuggestionDetails(suggestionId);
 
-  $("#primary").modal("show");
+    $("#primary").modal("show");
 }
 
 function View(id) {
-  var suggestionId = $(id).attr("suggestionId");
-  window.location.href = "/Admin/view-detialSuggestions?id=" + suggestionId;
+    var suggestionId = $(id).attr("suggestionId");
+    window.location.href = "/Admin/view-detialSuggestions?id=" + suggestionId;
 }
 
 function Filldatatable(data) {
-  $("#tblSuggestion").DataTable({
-    bAutoWidth: false,
-    data: data,
-    columns: [
-      {
-        visible: false,
-        data: "ComplainSuggestionId",
-      },
+    $("#tblSuggestion").DataTable({
+        bAutoWidth: false,
+        data: data,
+        columns: [{
+                visible: false,
+                data: "ComplainSuggestionId",
+            },
 
-      { data: "UserId" },
-      { data: "FullName" },
-      { data: "PhoneNumber" },
-      { data: "Status" },
-      { data: "AddedDate",
-      render: function(data) {
-        return new Date(data).toLocaleDateString("en-us");
-      } },
-      { data: "ModifiedBy",
-    
-    },
+            { data: "UserId" },
+            { data: "FullName" },
+            { data: "PhoneNumber" },
+            { data: "Status" },
+            {
+                data: "AddedDate",
+                render: function(data) {
+                    return new Date(data).toLocaleDateString("en-us");
+                }
+            },
+            {
+                data: "ModifiedBy",
 
-      {
-        mRender: function (data, type, row) {
-          return (
-            // '<a href="#" onclick="View(this)" suggestionId="' +
-            // row.ComplainSuggestionId +
-            // '" data-toggle="tooltip" data-placement="bottom" title="View FeedBack">' +
-            // ' <i class="bx bx-detail"></i></i></a>&nbsp; &nbsp;' 
-            // +
-            '<a href="#" onclick="ShowDetails(this)" suggestionId="' +
-            row.ComplainSuggestionId +
-            '" data-toggle="tooltip" data-placement="bottom" title="Show Details">' +
-            ' <i class="bx bxl-messenger"></i></i></a>'
-          );
-        },
-      },
-    ],
-    columnDefs: [
-      {
-        targets: "_all",
-        defaultContent: "",
-      },
-    ],
-    order: [[5, "desc"]],
-  });
+            },
+
+            {
+                mRender: function(data, type, row) {
+                    return (
+                        // '<a href="#" onclick="View(this)" suggestionId="' +
+                        // row.ComplainSuggestionId +
+                        // '" data-toggle="tooltip" data-placement="bottom" title="View FeedBack">' +
+                        // ' <i class="bx bx-detail"></i></i></a>&nbsp; &nbsp;' 
+                        // +
+                        '<a href="#" onclick="ShowDetails(this)" suggestionId="' +
+                        row.ComplainSuggestionId +
+                        '" data-toggle="tooltip" data-placement="bottom" title="Show Details">' +
+                        ' <i class="bx bxl-messenger"></i></i></a>'
+                    );
+                },
+            },
+        ],
+        columnDefs: [{
+            targets: "_all",
+            defaultContent: "",
+        }, ],
+        order: [
+            [6, "desc"]
+        ],
+    });
 }
 
 function SendReply(suggestionId) {
-  var url =
-    baseURL +
-    "PatientSuggestion/UpdateSuggestion?ComplainSuggestionId=" +
-    $("#hdnSuggestionId").val() +
-    "&Reply=" +
-    $("#txtSuggestion").val() +
-    "&modifyBy=" +
-    $(".user-name").find("strong").text();
+    var url =
+        baseURL +
+        "PatientSuggestion/UpdateSuggestion?ComplainSuggestionId=" +
+        $("#hdnSuggestionId").val() +
+        "&Reply=" +
+        $("#txtSuggestion").val() +
+        "&modifyBy=" +
+        $(".user-name").find("strong").text();
 
-  $.ajax({
-    url: url,
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    type: "POST",
-    datatype: "application/json",
-    contentType: "application/json; charset=utf-8",
-    data: "",
-    beforeSend: function () {
-      $.LoadingOverlay("show");
-    },
-    success: function (data, textStatus, xhr) {
-      $.LoadingOverlay("hide");
-      Swal.fire({
-        title: "Confirmation!",
-        text: "Reply send successfully",
-        type: "success",
-        confirmButtonClass: "btn btn-primary",
-        buttonsStyling: false,
-        confirmButtonText: "Ok",
-      }).then((result) => {
-        window.location.reload();
-      });
-    },
-    error: function (xhr, textStatus, err) {
-      if (xhr.status == "500" && xhr.statusText == "InternalServerError")
-        console.log(xhr.statusText);
-      else console.log(xhr.statusText);
-    },
-    complete: function (data) {
-      // GetAllSuggestions();
+    $.ajax({
+        url: url,
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        type: "POST",
+        datatype: "application/json",
+        contentType: "application/json; charset=utf-8",
+        data: "",
+        beforeSend: function() {
+            $.LoadingOverlay("show");
+        },
+        success: function(data, textStatus, xhr) {
+            $.LoadingOverlay("hide");
+            Swal.fire({
+                title: "Confirmation!",
+                text: "Reply send successfully",
+                type: "success",
+                confirmButtonClass: "btn btn-primary",
+                buttonsStyling: false,
+                confirmButtonText: "Ok",
+            }).then((result) => {
+                window.location.reload();
+            });
+        },
+        error: function(xhr, textStatus, err) {
+            if (xhr.status == "500" && xhr.statusText == "InternalServerError")
+                console.log(xhr.statusText);
+            else console.log(xhr.statusText);
+        },
+        complete: function(data) {
+            // GetAllSuggestions();
 
-      // Hide Loading
-      $.LoadingOverlay("hide");
-    },
-  });
+            // Hide Loading
+            $.LoadingOverlay("hide");
+        },
+    });
 }
 
 function GetSuggestionDetails(id) {
-  var url =
-    baseURL + "PatientSuggestion/GetSuggestionComplain?suggestionId=" + id;
-  $.ajax({
-    url: url,
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    type: "GET",
-    datatype: "application/json",
-    contentType: "application/json; charset=utf-8",
-    data: "",
-    beforeSend: function () {
-      $.LoadingOverlay("show");
-    },
-    success: function (data, textStatus, xhr) {
-      $.LoadingOverlay("hide");
-      $("#txtComplain").val(data.Complain);
-    },
-    error: function (xhr, textStatus, err) {
-      if (xhr.status == "500" && xhr.statusText == "InternalServerError")
-        console.log(xhr.statusText);
-      else console.log(xhr.statusText);
-    },
-    complete: function (data) {
-      // Hide Loading
-      $.LoadingOverlay("hide");
+    var url =
+        baseURL + "PatientSuggestion/GetSuggestionComplain?suggestionId=" + id;
+    $.ajax({
+        url: url,
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        type: "GET",
+        datatype: "application/json",
+        contentType: "application/json; charset=utf-8",
+        data: "",
+        beforeSend: function() {
+            $.LoadingOverlay("show");
+        },
+        success: function(data, textStatus, xhr) {
+            $.LoadingOverlay("hide");
+            $("#txtComplain").val(data.Complain);
+        },
+        error: function(xhr, textStatus, err) {
+            if (xhr.status == "500" && xhr.statusText == "InternalServerError")
+                console.log(xhr.statusText);
+            else console.log(xhr.statusText);
+        },
+        complete: function(data) {
+            // Hide Loading
+            $.LoadingOverlay("hide");
 
-      var inputValue = $(".form-input").val();
-      if (inputValue == "") {
-        $(".form-input").removeClass("filled");
-        $(".form-input").parents(".form-group").removeClass("focused");
-      } else {
-        $(".form-input").addClass("filled");
-        $(".form-input").parents(".form-group").addClass("focused");
-      }
-    },
-  });
+            var inputValue = $(".form-input").val();
+            if (inputValue == "") {
+                $(".form-input").removeClass("filled");
+                $(".form-input").parents(".form-group").removeClass("focused");
+            } else {
+                $(".form-input").addClass("filled");
+                $(".form-input").parents(".form-group").addClass("focused");
+            }
+        },
+    });
 }
--- services/docAppointmentServices.js
+++ services/docAppointmentServices.js
@@ -503,96 +503,96 @@ function ViewBookingDetails(PatientId, AppointmentId) {
     });
 }
 
-function CalculateAge(userinput) {
-    //collect input from HTML form and convert into date format
-    // var userinput = document.getElementById("txtInfoAge").value;
-    var dob = new Date(userinput);
+// function CalculateAge(userinput) {
+//     //collect input from HTML form and convert into date format
+//     // var userinput = document.getElementById("txtInfoAge").value;
+//     var dob = new Date(userinput);
 
-    //check user provide input or not
-    if (userinput == null || userinput == "") {
-        document.getElementById("spnAge").innerHTML = "**Choose a date please!";
-        return false;
-    }
+//     //check user provide input or not
+//     if (userinput == null || userinput == "") {
+//         document.getElementById("spnAge").innerHTML = "**Choose a date please!";
+//         return false;
+//     }
 
-    //execute if the user entered a date
-    else {
-        //extract the year, month, and date from user date input
-        var dobYear = dob.getYear();
-        var dobMonth = dob.getMonth();
-        var dobDate = dob.getDate();
+//     //execute if the user entered a date
+//     else {
+//         //extract the year, month, and date from user date input
+//         var dobYear = dob.getYear();
+//         var dobMonth = dob.getMonth();
+//         var dobDate = dob.getDate();
 
-        //get the current date from the system
-        var now = new Date();
-        //extract the year, month, and date from current date
-        var currentYear = now.getYear();
-        var currentMonth = now.getMonth();
-        var currentDate = now.getDate();
+//         //get the current date from the system
+//         var now = new Date();
+//         //extract the year, month, and date from current date
+//         var currentYear = now.getYear();
+//         var currentMonth = now.getMonth();
+//         var currentDate = now.getDate();
 
-        //declare a variable to collect the age in year, month, and days
-        var age = {};
-        var ageString = "";
+//         //declare a variable to collect the age in year, month, and days
+//         var age = {};
+//         var ageString = "";
 
-        //get years
-        yearAge = currentYear - dobYear;
+//         //get years
+//         yearAge = currentYear - dobYear;
 
-        //get months
-        if (currentMonth >= dobMonth)
-        //get months when current month is greater
-            var monthAge = currentMonth - dobMonth;
-        else {
-            yearAge--;
-            var monthAge = 12 + currentMonth - dobMonth;
-        }
+//         //get months
+//         if (currentMonth >= dobMonth)
+//         //get months when current month is greater
+//             var monthAge = currentMonth - dobMonth;
+//         else {
+//             yearAge--;
+//             var monthAge = 12 + currentMonth - dobMonth;
+//         }
 
-        //get days
-        if (currentDate >= dobDate)
-        //get days when the current date is greater
-            var dateAge = currentDate - dobDate;
-        else {
-            monthAge--;
-            var dateAge = 31 + currentDate - dobDate;
+//         //get days
+//         if (currentDate >= dobDate)
+//         //get days when the current date is greater
+//             var dateAge = currentDate - dobDate;
+//         else {
+//             monthAge--;
+//             var dateAge = 31 + currentDate - dobDate;
 
-            if (monthAge < 0) {
-                monthAge = 11;
-                yearAge--;
-            }
-        }
-        //group the age in a single variable
-        age = {
-            years: yearAge,
-            months: monthAge,
-            days: dateAge,
-        };
+//             if (monthAge < 0) {
+//                 monthAge = 11;
+//                 yearAge--;
+//             }
+//         }
+//         //group the age in a single variable
+//         age = {
+//             years: yearAge,
+//             months: monthAge,
+//             days: dateAge,
+//         };
 
-        if (age.years > 0 && age.months > 0 && age.days > 0)
-            ageString =
-            age.years +
-            " years, " +
-            age.months +
-            " months, and " +
-            age.days +
-            " days old.";
-        else if (age.years == 0 && age.months == 0 && age.days > 0)
-            ageString = age.days + " days old!";
-        //when current month and date is same as birth date and month
-        else if (age.years > 0 && age.months == 0 && age.days == 0)
-            ageString = age.years + " years old. Happy Birthday!!";
-        else if (age.years > 0 && age.months > 0 && age.days == 0)
-            ageString = age.years + " years and " + age.months + " months old.";
-        else if (age.years == 0 && age.months > 0 && age.days > 0)
-            ageString = age.months + " months and " + age.days + " days old.";
-        else if (age.years > 0 && age.months == 0 && age.days > 0)
-            ageString = age.years + " years, and" + age.days + " days old.";
-        else if (age.years == 0 && age.months > 0 && age.days == 0)
-            ageString = age.months + " months old.";
-        //when current date is same as dob(date of birth)
-        else ageString = "It's first day on Earth!";
+//         if (age.years > 0 && age.months > 0 && age.days > 0)
+//             ageString =
+//             age.years +
+//             " years, " +
+//             age.months +
+//             " months, and " +
+//             age.days +
+//             " days old.";
+//         else if (age.years == 0 && age.months == 0 && age.days > 0)
+//             ageString = age.days + " days old!";
+//         //when current month and date is same as birth date and month
+//         else if (age.years > 0 && age.months == 0 && age.days == 0)
+//             ageString = age.years + " years old. Happy Birthday!!";
+//         else if (age.years > 0 && age.months > 0 && age.days == 0)
+//             ageString = age.years + " years and " + age.months + " months old.";
+//         else if (age.years == 0 && age.months > 0 && age.days > 0)
+//             ageString = age.months + " months and " + age.days + " days old.";
+//         else if (age.years > 0 && age.months == 0 && age.days > 0)
+//             ageString = age.years + " years, and" + age.days + " days old.";
+//         else if (age.years == 0 && age.months > 0 && age.days == 0)
+//             ageString = age.months + " months old.";
+//         //when current date is same as dob(date of birth)
+//         else ageString = "It's first day on Earth!";
 
-        //display the calculated age
-        return document.getElementById("spnAge").innerHTML =
-            ageString;
-    }
-}
+//         //display the calculated age
+//         return document.getElementById("spnAge").innerHTML =
+//             ageString;
+//     }
+// }
 
 //====date formater by using mustache=====
 Mustache.Formatters = {
